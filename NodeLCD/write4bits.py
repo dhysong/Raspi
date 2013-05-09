004c8f788f0abbcdf41e541f5c13696eb031b390
dhysong-nci <dhysong@nci.com>
2013-05-09 08:25:48 -0400

    Working on py

--- NodeLCD/write4bits.py
+++ NodeLCD/write4bits.py
@@ -1,12 +1,11 @@
 
-
-write4bits(0x33) # initialization
-write4bits(0x32) # initialization
-write4bits(0x28) # 2 line 5x7 matrix
-write4bits(0x0C) # turn cursor off 0x0E to enable cursor
-write4bits(0x06) # shift cursor right
-
-
 def write4bits(self, bits, char_mode=False):
 	bits=bin(bits)[2:].zfill(8)
 	print bits
+
+write4bits(0x33) 
+write4bits(0x32) 
+write4bits(0x28) 
+write4bits(0x0C) 
+write4bits(0x06) 
+
