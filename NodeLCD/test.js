ae51d065ddce154194dbcd1e934a1dd79828d97e
dhysong-nci <dhysong@nci.com>
2013-05-12 06:53:41 -0400

    Working on py

--- NodeLCD/test.js
+++ NodeLCD/test.js
@@ -1,10 +1,12 @@
 var charLCD = require('CharLCD');
-console.log(charLCD);
 
-var lcd = charLCD.Setup();
-console.log(lcd);
-lcd.Begin(16,1);
-lcd.Clear();
-lcd.Message('Hello World');
-lcd.Message('Hello Drew');
+var lcd = charLCD.Setup(pinReadyCallback);
+
+function pinReadyCallback(){
+	console.log(lcd);
+	lcd.Begin(16,1);
+	lcd.Clear();
+	lcd.Message('Hello World');
+	lcd.Message('Hello Drew');
+}
 
